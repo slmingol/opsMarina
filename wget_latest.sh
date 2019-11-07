e5bd3d16fed0174bf4f030435c490659791e8519
Sam Mingolelli <github@jake8us.org>
2019-11-07 09:38:20 -0500

    WIP building artifacts

--- wget_latest.sh
+++ wget_latest.sh
@@ -3,26 +3,32 @@
 
 #https://github.com/Jmainguy/k8sCapcity/releases/latest
 
-while read -r line; do
-    basename=$(echo "$line" | awk -F/ '{print $NF}')
+download () {
+    # example URLs
     #https://github.com/Jmainguy/k8sCapcity/releases/download/v0.1.2/k8sCapcity_Darwin_x86_64.tar.gz
     #https://github.com/Jmainguy/k8sCapcity/releases/download/v0.1.2/k8sCapcity_Linux_x86_64.tar.gz
 
-    echo "${line}/releases/latest"
+    plat="$1"
+    basename="$2"
 
-    odir="artifacts/macos"
-    tball="${basename}_Darwin_x86_64.tar.gz"
-    mkdir -p $odir && (cd $odir; curl -sLC - -O "${line}/releases/latest/download/$tball")
-    file "$odir/$tball" | grep -q 'gzip compressed data' && \
-        tar zxvf "$odir/$tball" --exclude={LICENSE,README*} -C $odir/
+    [ "$plat" == "macos" ] && os="Darwin" || os="Linux"
 
-    odir="artifacts/linux"
-    tball="${basename}_Linux_x86_64.tar.gz"
+    odir="artifacts/$plat"
+    tball="${basename}_${os}_x86_64.tar.gz"
     mkdir -p $odir && (cd $odir; curl -sLC - -O "${line}/releases/latest/download/$tball")
     file "$odir/$tball" | grep -q 'gzip compressed data' && \
         tar zxvf "$odir/$tball" --exclude={LICENSE,README*} -C $odir/
 
     #tar ztvf k8sCapcity_Linux_x86_64.tar.gz --exclude={LICENSE,README*}
+
+}
+
+while read -r line; do
+    basename=$(echo "$line" | awk -F/ '{print $NF}')
+
+    echo "${line}/releases/latest"
+    download "macos" "$basename"
+    download "linux" "$basename"
 
 done < <(grep -E 'git|http' .meta  | awk -F': "' '{print $2}' | sed 's/".*$//')
 
