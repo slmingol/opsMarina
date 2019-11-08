9e878e9bf6a2571e914da83980b975aee27079bb
Sam Mingolelli <github@jake8us.org>
2019-11-08 00:02:53 -0500

    WIP - travis CI driven releases

--- wget_latest.sh
+++ wget_latest.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-#https://github.com/Jmainguy/k8sCapcity/releases/latest
+#-------------------------------------------------------------------------------------------------------
 
 download () {
     # example URLs
@@ -9,27 +8,34 @@ download () {
     #https://github.com/Jmainguy/k8sCapcity/releases/download/v0.1.2/k8sCapcity_Linux_x86_64.tar.gz
 
     plat="$1"
-    basename="$2"
+    line="$2"
 
     [ "$plat" == "macos" ] && os="Darwin" || os="Linux"
 
+    echo "OS: $plat -- ${line}/releases/latest"
+
     odir="artifacts/$plat"
+    basename=$(echo "$line" | awk -F/ '{print $NF}')
     tball="${basename}_${os}_x86_64.tar.gz"
     mkdir -p $odir && (cd $odir; curl -sLC - -O "${line}/releases/latest/download/$tball")
     file "$odir/$tball" | grep -q 'gzip compressed data' && \
         tar zxvf "$odir/$tball" --exclude={LICENSE,README*} -C $odir/
-
-    #tar ztvf k8sCapcity_Linux_x86_64.tar.gz --exclude={LICENSE,README*}
-
 }
 
-while read -r line; do
-    basename=$(echo "$line" | awk -F/ '{print $NF}')
+#-------------------------------------------------------------------------------------------------------
 
-    echo "${line}/releases/latest"
-    download "macos" "$basename"
-    download "linux" "$basename"
+while read -r line; do
+
+    download "macos" "$line"
+    download "linux" "$line"
+
+    find . -name "*.gz" -delete
 
 done < <(grep -E 'git|http' .meta  | awk -F': "' '{print $2}' | sed 's/".*$//')
+
+bundleDir="artifacts/bundles"
+mkdir -p $bundleDir
+tar -C artifacts/linux/ -zcvf $bundleDir/opsMarina_Linux_x86_64.tar.gz .
+tar -C artifacts/macos/ -zcvf $bundleDir/opsMarina_Darwin_x86_64.tar.gz .
 
 exit 0
